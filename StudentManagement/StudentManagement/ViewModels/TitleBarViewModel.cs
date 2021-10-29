4287897439e839d66c9a079bacf9d9fd73dd15ea
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-10-29 18:12:53 +0700

    Add TitleBar VM
    Signed-off-by: lehuutrung1412 <lehuutrung1412@users.noreply.github.com>

---
+++ StudentManagement/StudentManagement/ViewModels/TitleBarViewModel.cs
@@ -0,0 +1,53 @@
+﻿using StudentManagement.Commands;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace StudentManagement.ViewModels
+{
+    public class TitleBarViewModel : BaseViewModel
+    {
+
+        public ICommand CloseWindowCommand { get; set; }
+        public ICommand MaximizeWindowCommand { get; set; }
+        public ICommand MinimizeWindowCommand { get; set; }
+        public ICommand MouseMoveWindowCommand { get; set; }
+
+        public TitleBarViewModel()
+        {
+            CloseWindowCommand = new RelayCommand<UserControl>(
+                parameter => parameter != null,
+                parameter => Window.GetWindow(parameter)?.Close());
+
+            MaximizeWindowCommand = new RelayCommand<UserControl>(
+                parameter => parameter != null,
+                parameter =>
+                {
+                    Window parentWindow = Window.GetWindow(parameter);
+                    if (parentWindow != null)
+                    {
+                        parentWindow.WindowState = parentWindow.WindowState != WindowState.Maximized
+                            ? WindowState.Maximized
+                            : WindowState.Normal;
+                    }
+                });
+
+            MinimizeWindowCommand = new RelayCommand<UserControl>(
+                parameter => parameter != null,
+                parameter =>
+                {
+                    Window parentWindow = Window.GetWindow(parameter);
+                    if (parentWindow != null)
+                    {
+                        parentWindow.WindowState = parentWindow.WindowState != WindowState.Minimized
+                            ? WindowState.Minimized
+                            : WindowState.Normal;
+                    }
+                });
+
+            MouseMoveWindowCommand = new RelayCommand<UserControl>(
+                parameter => parameter != null,
+                parameter => Window.GetWindow(parameter)?.DragMove());
+        }
+    }
+}
