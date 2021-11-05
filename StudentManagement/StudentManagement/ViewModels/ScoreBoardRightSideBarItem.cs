a95d44913bcced91aa715eb1aeb9a635d8fb549b
htthtt12t1 <huathanh2503@gmail.com>
2021-11-05 21:58:54 +0700

    add ScoreBoard

---
+++ StudentManagement/StudentManagement/ViewModels/ScoreBoardRightSideBarItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static StudentManagement.ViewModels.ScoreBoardRightSideBar;
+
+namespace StudentManagement.ViewModels
+{
+    public class ScoreBoardRightSideBarItem : BaseViewModel
+    {
+        private DetailScore _currentItem;
+        public DetailScore CurrentItem { get => _currentItem; set => _currentItem = value; }
+
+        public ScoreBoardRightSideBarItem()
+        {
+            this.CurrentItem = null;
+        }
+
+        public ScoreBoardRightSideBarItem(DetailScore item)
+        {
+            this.CurrentItem = item;
+        }
+    }
+}
+
+
+
