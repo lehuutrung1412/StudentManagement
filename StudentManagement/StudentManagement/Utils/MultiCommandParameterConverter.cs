699076881744272f17d688dafa6901db0e4bc8d9
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-11-07 17:36:28 +0700

    Add multi command parameter converter
    Signed-off-by: lehuutrung1412 <lehuutrung1412@users.noreply.github.com>

---
+++ StudentManagement/StudentManagement/Utils/MultiCommandParameterConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace StudentManagement.Utils
+{
+    public class MultiCommandParameterConverter : MarkupExtension, IMultiValueConverter
+    {
+        private static MultiCommandParameterConverter _instance;
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values.Clone();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return _instance ?? (_instance = new MultiCommandParameterConverter());
+        }
+    }
+}
