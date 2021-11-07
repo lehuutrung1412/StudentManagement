72d45389eedcd4ba453ea6501720e1268ff9e1f7
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-11-07 18:30:30 +0700

    Edit and delete comment
    Signed-off-by: lehuutrung1412 <lehuutrung1412@users.noreply.github.com>

--- StudentManagement/StudentManagement/ViewModels/PostNewsfeedViewModel.cs
+++ StudentManagement/StudentManagement/ViewModels/PostNewsfeedViewModel.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,12 @@ namespace StudentManagement.ViewModels
 {
     public class PostNewsfeedViewModel : BaseViewModel
     {
+        private readonly CultureInfo _culture = new CultureInfo("vi-VN");
         public ICommand SendComment { get; set; }
         public ICommand ShowHideComments { get; set; }
         public ICommand ChangeImage { get; set; }
+        public ICommand DeleteComment { get; set; }
+        public ICommand EditComment { get; set; }
         public ObservableCollection<PostComment> PostComments { get; set; }
         public bool IsShowComments { get => _isShowComments; set { _isShowComments = value; OnPropertyChanged(); } }
         private bool _isShowComments;
@@ -33,10 +37,13 @@ namespace StudentManagement.ViewModels
 
         private int _imageIndex;
 
+        public Guid PostId { get; set; }
+
         public PostNewsfeedViewModel(string postText, DateTime postTime, ObservableCollection<string> stackImage)
         {
+            PostId = Guid.NewGuid();
             PostText = postText;
-            PostTime = postTime; //new DateTime(2021, 11, 3, 20, 25, 30);
+            PostTime = new DateTime(2021, 11, 1, 20, 25, 30);
             IsShowComments = true;
             StackPostImage = new ObservableCollection<string>(stackImage);
             ImageSelectedShow = StackPostImage?.Count > 0 ? StackPostImage[0] : null;
@@ -46,6 +53,8 @@ namespace StudentManagement.ViewModels
             SendComment = new RelayCommand<object>(_ => true, (p) => SendDraftComment(p));
             ShowHideComments = new RelayCommand<object>(_ => true, (p) => ShowHideAllComments(p));
             ChangeImage = new RelayCommand<object>(_ => true, (p) => ChangeImageToShow(p));
+            DeleteComment = new RelayCommand<Guid>(_ => true, (p) => DeleteOnComment(p));
+            EditComment = new RelayCommand<object>(_ => true, (p) => EditOnComment(p));
         }
 
         private void SendDraftComment(object comment)
@@ -53,7 +62,7 @@ namespace StudentManagement.ViewModels
             TextBox txbComment = comment as TextBox;
             if (txbComment.Text != "")
             {
-                PostComments.Add(new PostComment("Lê Hữu Trung", txbComment.Text));
+                PostComments.Add(new PostComment(Guid.NewGuid(), "Lê Hữu Trung", txbComment.Text, DateTime.Parse(DateTime.Now.ToString(), _culture)));
                 txbComment.Text = "";
             }
         }
@@ -72,17 +81,58 @@ namespace StudentManagement.ViewModels
                 : _imageIndex + 1 < StackPostImage.Count ? _imageIndex + 1 : StackPostImage.Count - 1;
             ImageSelectedShow = StackPostImage[_imageIndex];
         }
+
+        private void DeleteOnComment(Guid commentId)
+        {
+            try
+            {
+                PostComment commentToDelete = PostComments.Single(cmt => cmt.CommentId == commentId);
+                if (MyMessageBox.Show("Bạn có chắc chắn muốn xóa bình luận này không?", "Xóa bình luận", System.Windows.MessageBoxButton.YesNo, System.Windows.MessageBoxImage.Question) == System.Windows.MessageBoxResult.Yes)
+                {
+                    _ = PostComments.Remove(commentToDelete);
+                }
+            }
+            catch (Exception)
+            {
+                _ = MyMessageBox.Show("Đã có lỗi xảy ra, không thể xóa bình luận. Xin vui lòng thử lại", "Xóa bình luận", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
+            }
+        }
+
+        private void EditOnComment(object txbAndComment)
+        {
+            try
+            {
+                object[] values = (object[])txbAndComment;
+                TextBox textBox = values[0] as TextBox;
+                Guid commentId = (Guid)values[1];
+                PostComment commentToEdit = PostComments.Single(cmt => cmt.CommentId == commentId);
+                ControlTemplate template = textBox.Template;
+                TextBox childTextBox = (TextBox)template.FindName("txbComment", textBox);
+                childTextBox.Text = commentToEdit.Comment;
+                childTextBox.CaretIndex = childTextBox.Text.Length;
+                _ = childTextBox.Focus();
+                _ = PostComments.Remove(commentToEdit);
+            }
+            catch (Exception)
+            {
+                _ = MyMessageBox.Show("Đã có lỗi xảy ra, không thể chỉnh sửa bình luận. Xin vui lòng thử lại", "Sửa bình luận", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
+            }
+        }
     }
 
     public class PostComment
     {
+        public Guid CommentId { get; set; }
         public string Username { get; set; }
         public string Comment { get; set; }
+        public DateTime Time { get; set; }
 
-        public PostComment(string username, string comment)
+        public PostComment(Guid commentId, string username, string comment, DateTime time)
         {
+            CommentId = commentId;
             Username = username;
             Comment = comment;
+            Time = time;
         }
     }
 }
