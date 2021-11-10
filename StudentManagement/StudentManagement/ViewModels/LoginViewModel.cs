41bd9f40007d44b49879994dbcdc2a21d16361cd
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-11-10 17:11:54 +0700

    Update login viewmodel
    Signed-off-by: lehuutrung1412 <lehuutrung1412@users.noreply.github.com>

--- StudentManagement/StudentManagement/ViewModels/LoginViewModel.cs
+++ StudentManagement/StudentManagement/ViewModels/LoginViewModel.cs
@@ -1,12 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
+using System.ComponentModel;
 
 namespace StudentManagement.ViewModels
 {
-    public class LoginViewModel : BaseViewModel
+    public class LoginViewModel : BaseViewModel, INotifyDataErrorInfo
     {
+        private readonly ErrorBaseViewModel _errorBaseViewModel;
+
+        private string _username;
+        private string _password;
+
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+
+                // Validation
+                _errorBaseViewModel.ClearErrors();
+                if (!IsValid(Username))
+                {
+                    _errorBaseViewModel.AddError(nameof(Username), "Vui lòng nhập tên đăng nhập!");
+                }
+
+                OnPropertyChanged();
+            }
+        }
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+
+                // Validation
+                _errorBaseViewModel.ClearErrors();
+                if (!IsValid(Password))
+                {
+                    _errorBaseViewModel.AddError(nameof(Password), "Vui lòng nhập mật khẩu!");
+                }
+
+                OnPropertyChanged();
+            }
+        }
+
+        public bool CanLogin => !HasErrors;
+        public bool HasErrors => _errorBaseViewModel.HasErrors;
+
+        public LoginViewModel()
+        {
+            _errorBaseViewModel = new ErrorBaseViewModel();
+            _errorBaseViewModel.ErrorsChanged += ErrorBaseViewModel_ErrorsChanged;
+        }
+
+        public bool IsExistAccount()
+        {
+            if (Username == "admin" && Password == "1")
+            {
+                return true;
+            }
+            _ = MyMessageBox.Show("Tên đăng nhập hoặc mật khẩu không đúng!\nVui lòng thử lại!", "Đăng nhập thất bại", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
+            return false;
+        }
+
+        private bool IsValid(string propertyName)
+        {
+            return !string.IsNullOrEmpty(propertyName) && !string.IsNullOrWhiteSpace(propertyName);
+        }
+
+        private void ErrorBaseViewModel_ErrorsChanged(object sender, DataErrorsChangedEventArgs e)
+        {
+            ErrorsChanged?.Invoke(this, e);
+            OnPropertyChanged(nameof(CanLogin));
+        }
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            return _errorBaseViewModel.GetErrors(propertyName);
+        }
     }
 }
