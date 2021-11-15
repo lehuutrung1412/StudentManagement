0d090062a535931b4ddbf5c85beaf10c34ca3fe8
vinhqngo5@gmail.com <vinhqngo5@gmail.com>
2021-11-15 20:22:06 +0700

    Add dataprovider class for model

---
+++ StudentManagement/StudentManagement/Models/DataProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Models
+{
+    public class DataProvider
+    {
+        private static DataProvider s_instance;
+
+        public static DataProvider Instance => s_instance ?? (s_instance = new DataProvider());
+
+        private DataProvider()
+        {
+            Database = new StudentManagementEntities();
+        }
+
+        public StudentManagementEntities Database { get; set; }
+    }
+}
