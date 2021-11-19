95bb5b164b7e6d827acee146b2174099d12c3c24
vinhqngo5@gmail.com <vinhqngo5@gmail.com>
2021-11-19 09:55:03 +0700

    Moving Faculty Card class to objects

---
+++ StudentManagement/StudentManagement/Objects/FacultyCard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Objects
+{
+    public class FacultyCard : BaseObjectWithBaseViewModel, IBaseCard
+    {
+        private string _tenKhoa;
+        private DateTime _ngayThanhLap;
+        private int _soLuongSinhVien;
+        private string _cacHeDaoTao;
+
+        public FacultyCard() { }
+        public FacultyCard(string tenKhoa, DateTime ngayThanhLap, int soLuongSinhVien, string cacHeDaoTao)
+        {
+            _tenKhoa = tenKhoa;
+            _ngayThanhLap = ngayThanhLap;
+            _soLuongSinhVien = soLuongSinhVien;
+            _cacHeDaoTao = cacHeDaoTao;
+        }
+
+        public string TenKhoa { get => _tenKhoa; set => _tenKhoa = value; }
+        public DateTime NgayThanhLap { get => _ngayThanhLap; set => _ngayThanhLap = value; }
+        public int SoLuongSinhVien { get => _soLuongSinhVien; set => _soLuongSinhVien = value; }
+        public string CacHeDaoTao { get => _cacHeDaoTao; set => _cacHeDaoTao = value; }
+    }
+}
--- StudentManagement/StudentManagement/ViewModels/FacultyTrainingForm/AdminFacultyTrainingFormViewModel.cs
+++ StudentManagement/StudentManagement/ViewModels/FacultyTrainingForm/AdminFacultyTrainingFormViewModel.cs
@@ -15,32 +15,6 @@ namespace StudentManagement.ViewModels
 {
     public class AdminFacultyTrainingFormViewModel : BaseViewModel
     {
-        #region class
-        
-        public class FacultyCard : BaseObjectWithBaseViewModel, IBaseCard
-        {
-            private string _tenKhoa;
-            private DateTime _ngayThanhLap;
-            private int _soLuongSinhVien;
-            private string _cacHeDaoTao;
-
-            public FacultyCard() { }
-            public FacultyCard(string tenKhoa, DateTime ngayThanhLap, int soLuongSinhVien, string cacHeDaoTao)
-            {
-                _tenKhoa = tenKhoa;
-                _ngayThanhLap = ngayThanhLap;
-                _soLuongSinhVien = soLuongSinhVien;
-                _cacHeDaoTao = cacHeDaoTao;
-            }
-
-            public string TenKhoa { get => _tenKhoa; set => _tenKhoa = value; }
-            public DateTime NgayThanhLap { get => _ngayThanhLap; set => _ngayThanhLap = value; }
-            public int SoLuongSinhVien { get => _soLuongSinhVien; set => _soLuongSinhVien = value; }
-            public string CacHeDaoTao { get => _cacHeDaoTao; set => _cacHeDaoTao = value; }
-        }
-
-        #endregion
-
         #region properties
         static private ObservableCollection<FacultyCard> _storedFacultyCards;
         public static ObservableCollection<FacultyCard> StoredFacultyCards { get => _storedFacultyCards; set => _storedFacultyCards = value; }
